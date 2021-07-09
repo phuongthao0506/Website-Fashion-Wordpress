39c149fbcc65bc59fc53822bfde8d5e1a251dc81
phuongthao0506 <85739218+phuongthao0506@users.noreply.github.com>
2021-07-09 14:34:10 +0700

    d

--- wp-content/plugins/cloudinary-image-management-and-manipulation-in-the-cloud-cdn/js/gallery-block.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-components/build-style/style.css', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '9327c7730d65905612d47f796c42693c');
